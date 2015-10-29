4f91651015b2628c51c38710eb1525a7b5ae4f80
ahoymehearties <cyrusnemati@Cyruss-MacBook-Pro-2.local>
2015-10-29 09:59:57 -0700

    whatever you say, jefe

--- wp-content/themes/linstar-child/functions.php
+++ wp-content/themes/linstar-child/functions.php
@@ -6,3 +6,16 @@
 *
 */
 
+function themeslug_setup() {
+ 
+    ...
+ 
+    /**
+     * Add theme support for the Eventbrite API plugin.
+     * See: https://wordpress.org/plugins/eventbrite-api/
+     */
+    add_theme_support( 'eventbrite' );
+}
+add_action( 'after_setup_theme', 'themeslug_setup' );
+
+?>
