b39f30e45e2a778196e044f749ac2efbc508f7c7
ahoymehearties <cyrusnemati@Cyruss-MacBook-Pro-2.local>
2015-12-03 15:06:58 -0800

    I need MORE

--- wp-content/themes/linstar-child/functions.php
+++ wp-content/themes/linstar-child/functions.php
@@ -15,3 +15,4 @@ add_action( 'after_setup_theme', 'themeslug_setup' );
 add_image_size( 'sidebar-thumb', 272, 124, true ); // Theme-specific Mode
 add_image_size( 'frontpage-thumb', 275, 220, true ); //Theme-specific fp
 add_image_size( 'front-thumb2', 359, 220, true );  // Holy crap, another one
+add_image_size( 'full width', 960, 600, true ); //Larger size
