9b59ca131fcf581440de9702c9ec8522585c32a1
ahoymehearties <cyrusnemati@Cyruss-MacBook-Pro-2.local>
2016-03-22 10:42:12 -0700

    If I have to...

--- wp-content/themes/linstar/functions.php
+++ wp-content/themes/linstar/functions.php
@@ -8,6 +8,18 @@
 
 #Load core of theme
 include 'core/king.define.php';
+
+function themeslug_setup() {
+ 
+    add_theme_support( 'eventbrite' );
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'themeslug_setup' );
+add_image_size( 'square', 50, 50, true ); // Square for linked items
+add_image_size( 'sidebar-thumb', 272, 124, true ); // Theme-specific Mode
+add_image_size( 'frontpage-thumb', 275, 220, true ); //Theme-specific fp
+add_image_size( 'front-thumb2', 359, 220, true );  // Holy crap, another one
+add_image_size( 'full width', 960, 600, true ); //Larger size
 #
 #
 #	END of REGISTRATION
