96649a95bf35210597a5b624d48ff03b4bb15582
ahoymehearties <cyrusnemati@Cyruss-MacBook-Pro-2.local>
2016-04-01 12:54:49 -0700

    profile pic fix

--- wp-content/themes/pivot/functions.php
+++ wp-content/themes/pivot/functions.php
@@ -13,6 +13,7 @@ function themeslug_setup() {
 }
 add_action( 'after_setup_theme', 'themeslug_setup' );
 add_image_size( 'square', 50, 50, true ); // Square for linked items
+add_image_size( 'profile', 96, 96, true ); // For profile images
 add_image_size( 'sidebar-thumb', 272, 124, true ); // Theme-specific Mode
 add_image_size( 'frontpage-thumb', 275, 220, true ); //Theme-specific fp
 add_image_size( 'front-thumb2', 359, 220, true );  // Holy crap, another one
