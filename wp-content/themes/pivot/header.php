88db741feb21c2d271ab0a6185ab7df989ab839e
ahoymehearties <cyrusnemati@Cyruss-MacBook-Pro-2.local>
2016-04-20 11:57:50 -0700

    smoothscroll

--- wp-content/themes/pivot/header.php
+++ wp-content/themes/pivot/header.php
@@ -34,6 +34,11 @@ var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0049/1530.js?"+Math.floor(new Date().getTime()/3600000);
 a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 </script>
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded",function(){var e=function(){if("scrollingElement"in document)return document.scrollingElement;var a=document.documentElement,b=a.scrollTop;a.scrollTop=b+1;var c=a.scrollTop;a.scrollTop=b;return c>b?a:document.body}(),g=function(a){var b=e.scrollTop;if(2>a.length)a=-b;else if(a=document.querySelector(a)){a=a.getBoundingClientRect().top;var c=e.scrollHeight-window.innerHeight;a=b+a<c?a:c-b}else a=void 0;if(a)return new Map([["start",b],["delta",a]])},h=function(a){var b=
+a.getAttribute("href"),c=g(b);if(c){var d=new Map([["duration",800]]),k=performance.now();requestAnimationFrame(function l(a){d.set("elapsed",a-k);a=d.get("duration");var f=d.get("elapsed"),g=c.get("start"),h=c.get("delta");e.scrollTop=Math.round(h*(-Math.pow(2,-10*f/a)+1)+g);d.get("elapsed")<d.get("duration")?requestAnimationFrame(l):(history.pushState(null,null,b),e.scrollTop=c.get("start")+c.get("delta"))})}},f=document.querySelectorAll("a.scroll");(function b(c,d){var e=c.item(d);e.addEventListener("click",
+function(b){b.preventDefault();h(e)});if(d)return b(c,d-1)})(f,f.length-1)});
+</script>
 	<div id="main" class="layout-<?php 	
 		$mclass =  $king->cfg['layout']; 
 		if( !empty( $post->post_name ) ){
